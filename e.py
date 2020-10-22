bba80e0deb22276235a5a89b93a5a06fc35e8221
p1kac1u <67856006+p1kac1u@users.noreply.github.com>
2020-10-22 20:02:19 +0600

    Add files via upload

---
+++ e.py
@@ -0,0 +1,14 @@
+from decimal import Decimal, getcontext
+import math
+
+prec = int(input("Enter how many digits of e you want to be returned: "))
+getcontext().prec = prec + 1
+e = Decimal(1)
+error = Decimal(0)
+
+iteration = int(input("Enter how many times do you want e's formula to be iterated: "))
+
+for n in range(1, iteration + 1):
+  e += Decimal((1 / math.factorial(n)))
+  error = Decimal((abs(Decimal(math.e) - Decimal(e)))*100)
+  print(n, "|", e, "|", error, "%" )
