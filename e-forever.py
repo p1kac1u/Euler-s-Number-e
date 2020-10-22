365c95b8fcad918a60f0d8a831eddf800891c1bf
p1kac1u <67856006+p1kac1u@users.noreply.github.com>
2020-10-22 20:26:45 +0600

    Create e-forever.py

---
+++ e-forever.py
@@ -0,0 +1,16 @@
+3 run it in repl.it: https://repl.it/@p1kac1u/Eulers-Number-e#e-forever.py
+
+from decimal import Decimal, getcontext
+import math
+
+prec = int(input("Enter how many digits of e you want to be returned: "))
+getcontext().prec = prec + 1
+e = Decimal(1)
+
+iteration = int(input("Enter how many times do you want e's formula to be iterated: "))
+
+for n in range(1, iteration + 1):
+  e += Decimal((1 / math.factorial(n)))
+  error = Decimal((abs(Decimal(math.e) - Decimal(e)))*100)
+
+print(e)
