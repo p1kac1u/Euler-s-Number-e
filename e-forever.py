1ed3b252ef50e09a1fee80e7fda165dde6098a40
p1kac1u <67856006+p1kac1u@users.noreply.github.com>
2020-12-11 10:56:54 +0600

    Update e-forever.py

--- e-forever.py
+++ e-forever.py
@@ -1,5 +1,3 @@
-# run it in repl.it: https://repl.it/@p1kac1u/Eulers-Number-e#e-forever.py
-
 from decimal import Decimal, getcontext
 import math
 
@@ -11,4 +9,8 @@ iteration = int(input("Enter how many times do you want e's formula to be iterat
 
 for n in range(1, iteration + 1):
   e += Decimal((1 / math.factorial(n)))
-print(e)
+
+e_txt = open(r"digits_of_e.txt", "a")
+digitsofe = str(e)
+e_txt.write(digitsofe)
+e_txt.close()
